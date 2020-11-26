0602e74811a02a5b8a58d46e8d4b73dddc9d9195
zhangyuqiang <562655151@qq.com>
2020-11-26 18:18:43 +0800

    Hello World init

---
+++ hello/Hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"hello/world"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	u1 := uuid.Must(uuid.NewV4(), nil)
+	fmt.Printf("UUIDv4: %s\n", u1)
+
+	world.Say()
+}
